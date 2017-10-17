085a9e836e9d50dfa56fe197ae9e5570b48f4c34
MrHelei <1484542698@qq.com>
2017-10-17 18:50:39 +0800

    修改进度布局

--- app/src/main/java/com/sd/style/common/widget/HomeCircleView.java
+++ app/src/main/java/com/sd/style/common/widget/HomeCircleView.java
@@ -22,39 +22,38 @@ import com.sd.style.R;
 import com.sd.style.common.uitls.UIUtils;
 import com.sd.style.common.uitls.Utils;
 
-import static com.sd.style.R.string.current;
-
 
 /**
  * Created by Administrator on 2016/12/20.
  */
-public class InsideCircle extends View {
+public class HomeCircleView extends View {
 
-    public static final int Start_Degree = 155;
-    public static final int Total_Degree = 230;
-    public float Base_Degree;
     public static final String Normal_Color = "#55ffffff";
     public static final String Highlight_Color = "#ffffff";
     //小圆点半径
     public static final int Small_Point_Radius = UIUtils.dp2px(1);
     //大圆点半径
     public static final int Large_Point_Radius = UIUtils.dp2px(4);
+    public float Start_Degree;
+    public float Total_Degree;
+    public float Base_Degree;
+    public float Sin_Degree;
     //默认分成25等分
     public int part = 25;
     //平均夹角度数
-    public double pointDegree = Total_Degree / part;
+    public double pointDegree;
 
-    public InsideCircle(Context context) {
+    public HomeCircleView(Context context) {
         super(context);
         init();
     }
 
-    public InsideCircle(Context context, AttributeSet attrs) {
+    public HomeCircleView(Context context, AttributeSet attrs) {
         super(context, attrs);
         init();
     }
 
-    public InsideCircle(Context context, AttributeSet attrs, int defStyleAttr) {
+    public HomeCircleView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init();
     }
@@ -74,8 +73,8 @@ public class InsideCircle extends View {
     private int strokeWidth = dip2px(10);//进度条宽度
     private float total;//总进度
     private float progress;//当前进度
-    private int minDistance;//最小距离
-    private int maxDistance;//最大距离
+    private int minDistance = 0;//最小距离
+    private int maxDistance = 500;//最大距离
     private Bitmap mCarBitmap;
     private Bitmap fingerBitmap;
     private PointF point;
@@ -84,10 +83,16 @@ public class InsideCircle extends View {
         return progress;
     }
 
-    public void setProgress(float progress, float total) {
+    public void setProgress(float progress) {
         this.progress = progress;
+    }
+
+    public void setTotal(float total){
         this.total = total;
-        setProgressBgAnimation();
+    }
+
+    public void start(){
+        startProgressBgAnimation();
         requestLayout();
     }
 
@@ -100,8 +105,17 @@ public class InsideCircle extends View {
     }
 
     private void init() {
-        Base_Degree = (float) (90 - Math.toDegrees(5.0 / 13));
-        Logger.e("baseDegree------------" + Base_Degree);
+        Sin_Degree = (float) Math.toDegrees(5.0 / 13);
+        Base_Degree = 90 - Sin_Degree;
+        Start_Degree = Base_Degree + 90;
+        Total_Degree = 360 - Base_Degree * 2;
+        pointDegree = Total_Degree / part;
+        initPaint();
+        //指针坐标
+        point = new PointF();
+    }
+
+    private void initPaint() {
         strokePaint = new Paint();
         strokePaint.setAntiAlias(true);//抗锯齿，会消耗较大资源
         strokePaint.setStyle(Paint.Style.STROKE);
@@ -131,9 +145,6 @@ public class InsideCircle extends View {
         kmPaint.setAntiAlias(true);
         kmPaint.setStyle(Paint.Style.FILL);
         kmPaint.setColor(Color.WHITE);
-
-        //指针坐标
-        point = new PointF();
     }
 
     private Rect bounds = new Rect();
@@ -141,7 +152,6 @@ public class InsideCircle extends View {
     private int imageWidth = 80;
     private float outerRadius;
     private float centerRadius;
-    private float startDegree = 65;
     private float animationTotal = 5000;
     private float animationUpdate;
 
@@ -160,6 +170,22 @@ public class InsideCircle extends View {
         float verticalPadding = padding + strokeWidth / 2 + (bounds.height() - size) / 2f;
         mArcRect.set(bounds.left + horizontalPadding, bounds.top + verticalPadding,
                 bounds.right - horizontalPadding, bounds.bottom - verticalPadding);
+        //绘制进度条
+        drawProgress(canvas);
+        //绘制价格
+        drawIncome(canvas);
+        //画车
+        drawCar(canvas);
+        //画底部距离文字
+        drawDistance(canvas);
+        //画圆点
+        Logger.e("outerRadius-----" + outerRadius + "----centerRadius---" + centerRadius);
+        drawPoint(canvas);
+        //画指针
+        drawFinger(canvas);
+    }
+
+    private void drawProgress(Canvas canvas) {
         //绘制进度条背景
         strokePaint.setColor(Color.parseColor(Normal_Color));
         canvas.drawArc(mArcRect, Start_Degree, Total_Degree * (animationUpdate / animationTotal), false, strokePaint);
@@ -170,49 +196,52 @@ public class InsideCircle extends View {
             strokePaint.setColor(Color.parseColor(Highlight_Color));
             canvas.drawArc(mArcRect, Start_Degree, Total_Degree * currentPercent, false, strokePaint);
         }
+    }
 
-        //绘制价格
+    private void drawIncome(Canvas canvas) {
         textPaint.setTypeface(Typeface.createFromAsset(getContext().getAssets(), "fonts/DINCond-Black.otf"));
         textPaint.setTextSize(dip2px(72));
         String number = String.format("%.2f", progress);
         Rect numberRect = new Rect();
         textPaint.getTextBounds(number, 0, number.length(), numberRect);
-        canvas.drawText(number, bounds.centerX() - numberRect.width() / 2, bounds.centerY() + numberRect.height() / 2, textPaint);
+//        canvas.drawText(number, bounds.centerX() - numberRect.width() / 2, bounds.centerY() + numberRect.height() / 2, textPaint);
 
         //绘制顶部的高
         textPaint.setTypeface(Typeface.DEFAULT);
-        textPaint.setTextSize(dip2px(15));
-        String topContent = getContext().getResources().getString(current);
+        textPaint.setTextSize(dip2px(14));
+        String topContent = getContext().getResources().getString(R.string.current);
         Rect topRect = new Rect();
         textPaint.getTextBounds(topContent, 0, topContent.length(), topRect);
         float toptextX = bounds.centerX() - topRect.width() / 2;
         float toptextY = (float) (bounds.centerY() - UIUtils.dp2px(36) - UIUtils.dp2px(5));
         canvas.drawText(topContent, toptextX, toptextY, textPaint);
+    }
 
-        //画车
+    private void drawCar(Canvas canvas) {
         float carX = bounds.centerX() - mCarBitmap.getWidth() / 2;
-        float carY = (float) (bounds.centerY() + centerRadius * Math.sin(Math.toRadians(25)));
+        float carY = (float) (bounds.centerY() + centerRadius * Math.sin(Math.toRadians(Sin_Degree)));
         canvas.drawBitmap(mCarBitmap, carX, carY, bitmapPaint);
+    }
 
+    private void drawDistance(Canvas canvas) {
         //画底部最小距离文字
         kmPaint.setTextSize(dip2px(8));
-        String minKmContent = minDistance + " km";
+        String minKmContent = Utils.getContext().getString(R.string.km_placeholder, minDistance);
         Rect distanceRect = new Rect();
         kmPaint.getTextBounds(minKmContent, 0, minKmContent.length(), distanceRect);
-        float minKmX = (float) (bounds.centerX() - centerRadius * Math.cos(Math.toRadians(25)) - distanceRect.width() / 2 + strokeWidth / 2);
-        float minKmY = (float) (bounds.centerY() + centerRadius * Math.sin(Math.toRadians(25)) + distanceRect.height() * 2.5);
+        float minKmX = (float) (bounds.centerX() - centerRadius * Math.cos(Math.toRadians(Sin_Degree)) - distanceRect.width() / 2 + strokeWidth);
+        float minKmY = (float) (bounds.centerY() + centerRadius * Math.sin(Math.toRadians(Sin_Degree)) + distanceRect.height() * 3);
         canvas.drawText(minKmContent, minKmX, minKmY, kmPaint);
 
         //画底部最大距离文字
-        maxDistance = 500;
-        String maxKmContent = maxDistance + " km";
+        String maxKmContent = Utils.getContext().getString(R.string.km_placeholder, maxDistance);
         kmPaint.getTextBounds(maxKmContent, 0, maxKmContent.length(), distanceRect);
-        float maxKmX = (float) (bounds.centerX() + centerRadius * Math.cos(Math.toRadians(25)) - distanceRect.width() / 2 - strokeWidth / 2);
-        float maxKmY = (float) (bounds.centerY() + centerRadius * Math.sin(Math.toRadians(25)) + distanceRect.height() * 2.5);
+        float maxKmX = (float) (bounds.centerX() + centerRadius * Math.cos(Math.toRadians(Sin_Degree)) - distanceRect.width() / 2 - strokeWidth);
+        float maxKmY = (float) (bounds.centerY() + centerRadius * Math.sin(Math.toRadians(Sin_Degree)) + distanceRect.height() * 3);
         canvas.drawText(maxKmContent, maxKmX, maxKmY, kmPaint);
+    }
 
-        //画圆点
-        Logger.e("centerRadius------------" + centerRadius);
+    private void drawPoint(Canvas canvas) {
         for (float i = 0; i <= part; i++) {
             float pointX = (float) (bounds.centerX() - 0.9 * centerRadius * Math.sin(Math.toRadians(Base_Degree + i * pointDegree)));
             float pointY = (float) (bounds.centerY() + 0.9 * centerRadius * Math.cos(Math.toRadians(Base_Degree + i * pointDegree)));
@@ -226,14 +255,16 @@ public class InsideCircle extends View {
             }
             canvas.drawCircle(pointX, pointY, radius, circlePaint);
         }
+    }
 
+    private void drawFinger(Canvas canvas) {
         //画指针图片
         //定义矩阵对象
         Matrix matrix = new Matrix();
-        matrix.postRotate(startDegree + currentAngle + 180, bounds.centerX(), bounds.centerY());
+        matrix.postRotate(Base_Degree + currentAngle + 180, bounds.centerX(), bounds.centerY());
         Bitmap bitmap = Bitmap.createBitmap(fingerBitmap, 0, 0, fingerBitmap.getWidth(),
                 fingerBitmap.getHeight(), matrix, true);
-        canvas.drawBitmap(bitmap, point.x, point.y, bitmapPaint);
+        canvas.drawBitmap(bitmap, point.x - bitmap.getWidth()/2, point.y - bitmap.getHeight(), bitmapPaint);
     }
 
     private float currentAngle;
@@ -244,7 +275,7 @@ public class InsideCircle extends View {
     /**
      * 进度背景动画
      */
-    private void setProgressBgAnimation() {
+    public void startProgressBgAnimation() {
         if (valueAnimator == null) {
             valueAnimator = ValueAnimator.ofFloat(0, animationTotal);
             valueAnimator.setDuration(1200);
@@ -256,7 +287,6 @@ public class InsideCircle extends View {
                     animationUpdate = (float) animation.getAnimatedValue();
                     invalidate();
                 }
-
 
             });
 
@@ -303,7 +333,7 @@ public class InsideCircle extends View {
     private void setFingerAnimation(float start, float end) {
         if (fingerValueAnimator == null) {
             fingerValueAnimator = ValueAnimator.ofFloat(start, end);
-            fingerValueAnimator.setDuration(5000);
+            fingerValueAnimator.setDuration(2000);
             fingerValueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
                 @Override
                 public void onAnimationUpdate(ValueAnimator animation) {
@@ -311,10 +341,9 @@ public class InsideCircle extends View {
                     currentAngle = currentPercent * Total_Degree;
                     //计算指针的坐标
                     Logger.e("currentAngle-----------" + currentAngle);
-                    point.x = (float) (bounds.centerX() - outerRadius * Math.sin(Math.toRadians(startDegree + currentAngle)));
-                    point.y = (float) (bounds.centerY() + outerRadius * Math.cos(Math.toRadians(startDegree + currentAngle)));
+                    point.x = (float) (bounds.centerX() - outerRadius * Math.sin(Math.toRadians(Base_Degree + currentAngle)));
+                    point.y = (float) (bounds.centerY() + outerRadius * Math.cos(Math.toRadians(Base_Degree + currentAngle)));
                     //进度重绘
-
                     invalidate();
                 }
             });
--- app/src/main/java/com/sd/style/module/home/v/fragment/HomeFragment.java
+++ app/src/main/java/com/sd/style/module/home/v/fragment/HomeFragment.java
@@ -3,7 +3,7 @@ package com.sd.style.module.home.v.fragment;
 import com.sd.style.R;
 import com.sd.style.common.base.BaseFragment;
 import com.sd.style.common.base.BasePresenter;
-import com.sd.style.common.widget.InsideCircle;
+import com.sd.style.common.widget.HomeCircleView;
 
 import butterknife.BindView;
 
@@ -14,7 +14,7 @@ import butterknife.BindView;
 public class HomeFragment extends BaseFragment{
 
     @BindView(R.id.inside_circle)
-    InsideCircle inside_circle;
+    HomeCircleView inside_circle;
 
     public static HomeFragment newInstance(){
         return new HomeFragment();
@@ -32,7 +32,11 @@ public class HomeFragment extends BaseFragment{
 
     @Override
     protected void bindData() {
-        inside_circle.setProgress(0, 80);
+        inside_circle.setMinDistance(0);
+        inside_circle.setMaxDistance(500);
+        inside_circle.setProgress(80);
+        inside_circle.setTotal(100);
+        inside_circle.start();
     }
 
     @Override
