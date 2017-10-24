caeba62420a06b8245705fe66656f80923bd172d
MrHelei <1484542698@qq.com>
2017-10-24 17:55:19 +0800

    waveView

---
+++ app/src/main/java/com/sd/style/common/widget/WaveView.java
@@ -0,0 +1,22 @@
+package com.sd.style.common.widget;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Author: HeLei on 2017/10/24 16:57
+ * 水波纹view
+ */
+
+public class WaveView extends View {
+    public WaveView(Context context) {
+        this(context, null);
+    }
+
+    public WaveView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
