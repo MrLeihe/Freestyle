548ea7999a6011284a535846cdc1268cd6693786
MrHelei <1484542698@qq.com>
2017-10-25 13:34:23 +0800

    完成了对waveView的功能编写

--- app/src/main/java/com/sd/style/common/widget/WaveView.java
+++ app/src/main/java/com/sd/style/common/widget/WaveView.java
@@ -3,9 +3,21 @@ package com.sd.style.common.widget;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
+
+import com.orhanobut.logger.Logger;
+import com.sd.style.common.uitls.ContextCompactUtils;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Author: HeLei on 2017/10/24 16:57
@@ -14,11 +26,25 @@ import android.view.View;
 
 public class WaveView extends View {
 
-
-    private long mDuration;
+    private static final int CREATE_CIRCLE = 0;
+    //每个圆存在的时间
+    private long mDuration = 2000;
+    //最小半径
     private float minWaveRadius;
+    //最大半径
     private float maxWaveRadius;
     private Paint paint;
+    //是否主动设置过最大半径
+    private boolean isSetMaxRadius;
+    //正在执行中的状态
+    private boolean isRunning;
+    private List<Circle> mCircles = new ArrayList<>();
+    //每隔speedTime创建圆
+    private long speedTime = 200;
+    //上个圆创建的时间
+    private long mLastCreateTime;
+    private Rect bounds = new Rect();
+    private Interpolator mInterpolator;
 
     public WaveView(Context context) {
         this(context, null);
@@ -37,27 +63,139 @@ public class WaveView extends View {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-
+        if (!isSetMaxRadius) {
+            maxWaveRadius = Math.min(w, h) / 2;
+        }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        getDrawingRect(bounds);
+        Iterator<Circle> iterator = mCircles.iterator();
+        while (iterator.hasNext()) {
+            Circle circle = iterator.next();
+            //超过duration时间的圆移除掉
+            if (System.currentTimeMillis() - circle.getCreateTime() < mDuration) {
+                paint.setAlpha(circle.getAlpha());
+                int width = getWidth() / 2;
+                int centerX = bounds.centerX();
+                Logger.e("width---------->" + width + "---centerX-------->" + centerX);
+                canvas.drawCircle(getWidth() / 2, getHeight() / 2, circle.getWaveRadius(), paint);
+            } else {
+                iterator.remove();
+            }
+        }
+        if (mCircles.size() > 0) {
+            postInvalidateDelayed(10);
+        }
     }
 
+
+    private Handler handler = new Handler(){
+        public void handleMessage(Message msg){
+            switch (msg.what) {
+                case CREATE_CIRCLE :
+                    removeMessages(CREATE_CIRCLE);
+                    if(isRunning) {
+                        createCircle();
+                        sendEmptyMessageDelayed(CREATE_CIRCLE, speedTime);
+                    }
+                    break;
+            }
+        }
+    };
+
+    private void createCircle() {
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - mLastCreateTime < speedTime) {
+            return;
+        }
+        Circle circle = new Circle();
+        mCircles.add(circle);
+        invalidate();
+        //更新最后创建时间
+        mLastCreateTime = currentTime;
+    }
+
+    /**
+     * 启动
+     */
+    public void start() {
+        if (!isRunning) {
+            isRunning = true;
+            handler.sendEmptyMessage(CREATE_CIRCLE);
+        }
+    }
+
+    /**
+     * 暂停
+     */
+    public void pause() {
+        isRunning = false;
+    }
+
+    /**
+     * 销毁
+     */
+    public void destroy(){
+        isRunning = false;
+        if(handler != null) {
+            handler.removeCallbacksAndMessages(null);
+        }
+    }
+
+    /**
+     * 设置一个圆存在的时间
+     * @param duration  long时间
+     */
     public void setDuration(long duration) {
         mDuration = duration;
     }
 
+    /**
+     * 设置初始的圆半径
+     * @param minWaveRadius 半径
+     */
     public void setMinWaveRadius(float minWaveRadius) {
         this.minWaveRadius = minWaveRadius;
     }
 
+    /**
+     * 设置最大半径
+     * @param maxWaveRadius 半径
+     */
     public void setMaxWaveRadius(float maxWaveRadius) {
         this.maxWaveRadius = maxWaveRadius;
+        isSetMaxRadius = true;
     }
 
-    private class Circle{
+    /**
+     * 设置圆的填充颜色
+     * @param color int
+     */
+    public void setColor(int color) {
+        paint.setColor(ContextCompactUtils.getColor(getContext(), color));
+    }
+
+    /**
+     * 设置插值器
+     */
+    public void setInterpolator(Interpolator interpolator){
+        mInterpolator = interpolator;
+        if(interpolator == null) {
+            mInterpolator = new LinearInterpolator();
+        }
+    }
+
+    /**
+     * paint style
+     */
+    public void setStyle(Paint.Style style){
+        paint.setStyle(style);
+    }
+
+    private class Circle {
         //圆创建的时间
         private long mCreateTime;
 
@@ -65,14 +203,20 @@ public class WaveView extends View {
             mCreateTime = System.currentTimeMillis();
         }
 
-        public int getAlpha(){
+        //圆的透明度
+        public int getAlpha() {
             float percent = (System.currentTimeMillis() - mCreateTime) * 1.0f / mDuration;
-            return (int) ((1.0f - percent) * 255);
+            return (int) ((1.0f - mInterpolator.getInterpolation(percent)) * 255);
         }
 
-        public float getWaveRadius(){
+        //圆变化的半径
+        public float getWaveRadius() {
             float percent = (System.currentTimeMillis() - mCreateTime) * 1.0f / mDuration;
-            return minWaveRadius + percent * (maxWaveRadius - minWaveRadius);
+            return minWaveRadius + mInterpolator.getInterpolation(percent) * (maxWaveRadius - minWaveRadius);
+        }
+
+        public long getCreateTime() {
+            return mCreateTime;
         }
     }
 }
--- app/src/main/java/com/sd/style/module/mine/v/fragment/MineFragment.java
+++ app/src/main/java/com/sd/style/module/mine/v/fragment/MineFragment.java
@@ -2,11 +2,14 @@ package com.sd.style.module.mine.v.fragment;
 
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.view.animation.LinearOutSlowInInterpolator;
 
 import com.sd.style.R;
 import com.sd.style.common.base.BaseFragment;
 import com.sd.style.common.base.BasePresenter;
+import com.sd.style.common.uitls.UIUtils;
 import com.sd.style.common.widget.SaleProgressView;
+import com.sd.style.common.widget.WaveView;
 
 import butterknife.BindView;
 
@@ -18,6 +21,8 @@ public class MineFragment extends BaseFragment {
 
     @BindView(R.id.sale_view)
     SaleProgressView sale_view;
+    @BindView(R.id.wave_view)
+    WaveView wave_view;
 
     public static MineFragment newInstance() {
         return new MineFragment();
@@ -35,7 +40,13 @@ public class MineFragment extends BaseFragment {
 
     @Override
     protected void bindData() {
-        handler.sendEmptyMessageDelayed(0, 100);
+        handler.sendEmptyMessage(0);
+        wave_view.setDuration(2000);
+        wave_view.setMinWaveRadius(UIUtils.dp2px(1));
+        wave_view.setMaxWaveRadius(UIUtils.dp2px(50));
+        wave_view.setInterpolator(new LinearOutSlowInInterpolator());
+        wave_view.setColor(R.color.theme_color);
+        wave_view.start();
     }
 
     private int progress;
@@ -55,4 +66,21 @@ public class MineFragment extends BaseFragment {
         return null;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        wave_view.start();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        wave_view.pause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        wave_view.destroy();
+    }
 }
